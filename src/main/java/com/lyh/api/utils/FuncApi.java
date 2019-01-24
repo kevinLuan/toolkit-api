2b674214df553152c82b3adf7d4902d204e34835
shoushen <shoushen@yzyw-inc.com>
2019-01-24 23:22:21 +0800

    增加function Api

---
+++ src/main/java/com/lyh/api/utils/FuncApi.java
@@ -0,0 +1,23 @@
+package com.lyh.api.utils;
+
+public class FuncApi {
+
+  @FunctionalInterface
+  public static interface FuncV {
+    public void apply();
+  }
+  @FunctionalInterface
+  public static interface FuncArgV<P> {
+    public void apply(P p);
+  }
+
+  @FunctionalInterface
+  public static interface FuncArgR<P, R> {
+    public R apply(P p);
+  }
+
+  @FunctionalInterface
+  public static interface FuncR<R> {
+    public R apply();
+  }
+}
